d032ea504683cfd53d88150cd3055b4531137256
Alexis Halbot-Schoonaert <a.halbotschoonaert@gmail.com>
2025-09-25 16:10:23 +0000

    Config des librairies locales + param des dags

--- dags/debug.py
+++ dags/debug.py
@@ -5,7 +5,7 @@ from metier.donnees_rt import traiter_donnees_rt
 
 utils.definir_en_test()
 
-#configurer_bdd()
-#telecharger_donnees_statiques()
-#enregistrer_donnees_statiques_en_bdd()
+configurer_bdd()
+telecharger_donnees_statiques()
+enregistrer_donnees_statiques_en_bdd()
 traiter_donnees_rt()
